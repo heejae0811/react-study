5111a9ba6508ef896b103578ce4d8968433c50f6
heejaelee <hj.lee2@stickint.kr>
2022-12-09 17:29:17 +0900

    swiper 패키지 설치

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,4 +1,10 @@
 import { Link } from 'react-router-dom'
+import { Navigation, Pagination, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 function MainPage() {
   return (
@@ -12,6 +18,38 @@ function MainPage() {
           <Link to="/study">스터디 페이지</Link>
         </li>
       </ul>
+
+      <Swiper
+        modules={[Navigation, Pagination, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide style={{ backgroundImage: `url("/images/profile01.jpg")` }}>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/profile02.jpg" alt="프로필사진"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/profile03.jpg" alt="프로필사진"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/profile04.jpg" alt="프로필사진"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/profile05.jpg" alt="프로필사진"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/profile06.jpg" alt="프로필사진"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/profile07.png" alt="프로필사진"/>
+        </SwiperSlide>
+        ...
+      </Swiper>
     </div>
   );
 }
