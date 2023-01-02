46a9b7b1411cca8398bc4b8884f407f82c49430d
heejaelee <hj.lee2@stickint.kr>
2023-01-02 13:46:40 +0900

    노마드 눈 내리기

--- src/pages/snow/index.js
+++ src/pages/snow/index.js
@@ -1,7 +1,36 @@
+import './index.scss'
+
 function SnowPage() {
+  const body = document.querySelector('body')
+
+  const  makeSnowflake = () => {
+    const snowflake = document.createElement('div')
+    const delay = Math.random() * 10
+    const initialOpacity = Math.random()
+    const MIN_DURATION = 10
+    const duration = Math.random() * 20 + MIN_DURATION
+
+    snowflake.classList.add('snowflake')
+    snowflake.style.left = `${Math.random() * window.screen.width}px`
+    snowflake.style.animationDelay = `${delay}s`
+    snowflake.style.opacity = initialOpacity
+    snowflake.style.animation = `fall ${duration}s linear`
+
+    body.appendChild(snowflake)
+
+    setTimeout(() => {
+      body.removeChild(snowflake)
+      makeSnowflake()
+    }, (duration + delay) * 1000)
+  }
+
+  for(let index = 0; index < 50; index++) {
+    setTimeout(makeSnowflake, 500 * index)
+  }
+
   return (
     <div className="snow-page">
-      눈내리기
+      <div className="snowflake"></div>
     </div>
   )
 }
